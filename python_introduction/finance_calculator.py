20a18378dc2e689315f6987bd5bd46124ebcb50e
jackobhat <jackobhat911@gmail.com>
2024-06-23 12:04:52 +0300

    financial calculator fixing error 01

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,5 +3,5 @@ monthly_expense = int((input("Enter your montly expense:")))
 monthly_saving = monthly_income - monthly_expense
 projected_savings = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
 
-print("your montly savings are $", monthly_saving)
-print("Projected savings after one year, with interest, is:",projected_savings)
+print(f"your montly savings are ${monthly_saving}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}")
